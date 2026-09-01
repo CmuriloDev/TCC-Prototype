ef6902aca648f29903c153d5aa8f9669ff71ae8b
Carlos Murilo Nogueira Portela <murilo.portela.dev@gmail.com>
2026-09-01 19:10:34 -0300

    Adiciona endpoint POST /api/resumo no ResumoController

--- prototype/src/main/java/com/adapter/prototype/controller/ResumoController.java
+++ prototype/src/main/java/com/adapter/prototype/controller/ResumoController.java
@@ -1,7 +1,27 @@
 package com.adapter.prototype.controller;
 
+import com.adapter.prototype.dto.ResumoRequest;
+import com.adapter.prototype.dto.ResumoResponse;
+import com.adapter.prototype.service.ResumoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/resumo")
 public class ResumoController {
+
+    private final ResumoService resumoService;
+
+    public ResumoController(ResumoService resumoService) {
+        this.resumoService = resumoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResumoResponse> gerarResumo(@RequestBody ResumoRequest request) {
+        ResumoResponse response = resumoService.gerarResumo(request);
+        return ResponseEntity.ok(response);
+    }
 }
