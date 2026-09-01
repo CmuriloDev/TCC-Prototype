f7009c13dcea540880036f0c32101a09f22e47a0
Carlos Murilo Nogueira Portela <murilo.portela.dev@gmail.com>
2026-09-01 19:09:35 -0300

    Adiciona excecao customizada TextoInvalidoException

---
+++ prototype/src/main/java/com/adapter/prototype/exception/TextoInvalidoException.java
@@ -0,0 +1,8 @@
+package com.adapter.prototype.exception;
+
+public class TextoInvalidoException extends RuntimeException {
+
+    public TextoInvalidoException(String mensagem) {
+        super(mensagem);
+    }
+}
