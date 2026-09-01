258d560c602f2e85bc7d6af441bf8081e66f18e3
Carlos Murilo Nogueira Portela <murilo.portela.dev@gmail.com>
2026-09-01 19:45:57 -0300

    Adiciona tratamento global de TextoInvalidoException retornando 400 com ErroResponse

---
+++ prototype/src/main/java/com/adapter/prototype/dto/ErroResponse.java
@@ -0,0 +1,21 @@
+package com.adapter.prototype.dto;
+
+public class ErroResponse {
+
+    private String mensagem;
+
+    public ErroResponse() {
+    }
+
+    public ErroResponse(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
---
+++ prototype/src/main/java/com/adapter/prototype/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.adapter.prototype.exception;
+
+import com.adapter.prototype.dto.ErroResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TextoInvalidoException.class)
+    public ResponseEntity<ErroResponse> tratarTextoInvalido(TextoInvalidoException ex) {
+        ErroResponse corpo = new ErroResponse(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(corpo);
+    }
+}
